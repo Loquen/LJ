6b280a1f33ef8deebf4ea799de0456a15a1a57d7
Loquen Jones <lrjones416@gmail.com>
2018-04-05 17:03:33 -0700

    added code for hiding and showing sections

--- js/main.js
+++ js/main.js
@@ -1,12 +1,58 @@
-window.onscroll = function() {navScroll()};
+$(document).ready(function(){
+  $("#home").show();
+  $("#web").hide();
+  $("#photo").hide();
+  $("#graphic").hide();
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+  //loquen nav link
+  $("#nav-jumbo").click(function(){
+    //first hide all unused sections
+    $("#web").hide();
+    $("#photo").hide();
+    $("#graphic").hide();
+    //then display jumbo div
+    $("#home").show();
+  });
 
-function navScroll() {
-  if(window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+  //web nav link
+  $("#nav-web").click(function(){
+    //first hide all unused sections
+    $("#home").hide();
+    $("#photo").hide();
+    $("#graphic").hide();
+    //then display web div
+    $("#web").show();
+  });
+
+
+  //explore button
+  $("#explore").click(function(){
+    //first hide all unused sections
+    $("#home").hide();
+    $("#photo").hide();
+    $("#graphic").hide();
+    //then display web div
+    $("#web").show();
+  });
+
+  //photo nav link
+  $("#nav-photo").click(function(){
+    //first hide all unused sections
+    $("#home").hide();
+    $("#web").hide();
+    $("#graphic").hide();
+    //then display photo div
+    $("#photo").show();
+  });
+
+  //graphic nav link
+  $("#nav-graphic").click(function(){
+    //first hide all unused sections
+    $("#home").hide();
+    $("#photo").hide();
+    $("#web").hide();
+    //then display graphic div
+    $("#graphic").show();
+  });
+
+});
